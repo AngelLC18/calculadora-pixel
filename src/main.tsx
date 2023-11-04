ad0541818cc6b75c1cbcb9856471adc94c33c6a1
Angel Luciano Ciancio <ciancioangel.epet20@gmail.com>
2023-11-04 10:01:51 -0300

    Añadiendo archivos al repositorio

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { DisplayProvider } from './providers/DisplayProvider/index.tsx'
+import { FlashDisplayProvider } from './providers/FlashDisplayProvider/index.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <DisplayProvider>
+        <FlashDisplayProvider>
+          <App />
+        </FlashDisplayProvider>
+    </DisplayProvider>
+  </React.StrictMode>,
+)
